1ea648b32115d3b474c16a3af39b17136ce7c81a
mlryckman27 <mlryckman27@gmail.com>
2023-04-19 20:01:01 -0400

    Adds predefined challenges to the database upon running app.py

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask
+from flask import render_template
+from flask import request
+from redis import Redis
+import random
+
+
+app = Flask(__name__)
+redis = Redis(host='localhost', port=6379, db=1)
+
+
+challenge_list = ['challenge 1',
+                  'challenge 2',
+                  'challenge 3',
+                  'challenge 4',
+                  'challenge 5',
+                  'challenge 6',
+                  'challenge 7']
+
+redis.delete('total_savings')
+redis.delete('challenge')
+for elem in challenge_list:
+    redis.lpush('challenge', elem)
+
+
+@app.route('/')
+@app.route('/index.html')
+def index():
+    return render_template('index.html', username="User")
+
+
+@app.route("/", methods=['POST'])
+def submit_spending():
+    amount = float(request.form['spending'])
+    item = str(request.form['item'])
+    temp = redis.get('total_savings')
+    if temp is None:
+        total_savings = 0.0
+    else:
+        total_savings = float(temp)
+
+    total_savings += amount
+    redis.set('total_savings', total_savings)
+
+    rand_challenge = random.randint(0, redis.llen('challenge'))
+    random_challenge = redis.lindex('challenge', rand_challenge)
+
+    return render_template('index.html', total_savings=total_savings, amount=amount, item=item, random_challenge=random_challenge)
+
+
+if __name__ == '__main__':
+    app.run()
