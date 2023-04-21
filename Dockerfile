35daa537f38215bd1920ce9e6ff352c1018daae8
mlryckman27 <mlryckman27@gmail.com>
2023-04-21 09:28:08 -0400

    updated comments

--- Dockerfile
+++ Dockerfile
@@ -5,4 +5,4 @@ RUN apk add --no-cache gcc musl-dev linux-headers
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 COPY . .
-CMD ["flask", "run"]
+CMD ["flask", "run", "--host", "0.0.0.0"]
--- app.py
+++ app.py
@@ -4,11 +4,11 @@ from flask import request
 from redis import Redis
 import random
 
-
+# Create instances of Flask and Redis
 app = Flask(__name__)
 redis = Redis(host='redis', port=6379)
 
-
+# Healthy activities to be stored in the redis database
 challenge_list = ['Activity 1: Take a 30-minute walk.',
                   'Activity 2: Do 20 sit-ups.',
                   'Activity 3: Read one chapter of a book you haven\'t yet read.',
@@ -17,18 +17,27 @@ challenge_list = ['Activity 1: Take a 30-minute walk.',
                   'Activity 6: Pick one room that needs to be cleaned the most, spend 45 minutes cleaning it.',
                   'Activity 7: Create 10-minute video blog about one of your favorite hobbies or interests.']
 
+# Since this is a test application,
+# delete keys and start fresh every
+# time the application executes,
+# then push the activities list into the database
 redis.delete('total_savings')
 redis.delete('challenge')
 for elem in challenge_list:
     redis.lpush('challenge', elem)
 
 
+# Render the template that serves as the main page
 @app.route('/')
 @app.route('/index.html')
 def index():
     return render_template('index.html')
 
 
+# Take in user data regarding item and value
+# Update the total savings and get a new
+# alternative healthy activity for the user
+# from the database and render it to the main page
 @app.route("/", methods=['POST'])
 def submit_spending():
     amount = float(request.form['spending'])
@@ -49,5 +58,5 @@ def submit_spending():
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run()
 
